9d99d6d490a1b595b0569550d6459e2999398bb0
Culurio <claudiohcx@gmail.com>
2023-03-21 00:56:49 +0000

    Exception corrigida

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -22,7 +22,6 @@ class Carro(identificador: String,  val motor:Motor) : Veiculo(identificador),Li
 
     override fun moverPara(x: Int, y: Int) {
         posicao.alterarPosicaoPara(x,y)
-        motor.desligar()
     }
 
     override fun toString(): String {
